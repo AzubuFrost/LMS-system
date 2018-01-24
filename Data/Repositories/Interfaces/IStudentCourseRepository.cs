746ac93d7184781b6c9b7aff101c17d08c058768
JumpInPoclet <798209845@qq.com>
2018-01-25 10:50:08 +1100

    StudentCourseRepository,LectureCourseRepository

---
+++ Data/Repositories/Interfaces/IStudentCourseRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Dto;
+using Model.Model;
+
+namespace Data.Repositories.Interfaces
+{
+    public interface IStudentCourseRepository : IGenericRepository<StudentCourse>
+    {
+        
+    }
+}
+
